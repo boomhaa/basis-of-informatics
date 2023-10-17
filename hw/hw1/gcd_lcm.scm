6ea1857220cd38abfaa8b1dabfa5b3e383c8f165
boomhaa <90379111+boomhaa@users.noreply.github.com>
2023-10-17 11:56:20 +0300

    scheme

--- hw/hw1/gcd_lcm.scm
+++
@@ -1,7 +0,0 @@
-(define (my-gcd a b)
-  (if (= b 0)
-      a
-      (my-gcd b (modulo a b))))
-
-(define (my-lcm a b)
-  (/ (* a b) (my-gcd a b)))
--- hw/hw1/gcd_lcm_prime.scm
+++ hw/hw1/gcd_lcm_prime.scm
@@ -1,3 +1,11 @@
+(define (my-gcd a b)
+  (if (= b 0)
+      a
+      (my-gcd b (modulo a b))))
+
+(define (my-lcm a b)
+  (/ (* a b) (my-gcd a b)))
+
 (define (prime? n)
   (define (inner_prime? n d)
     (or (or (= n d) (< n 2)) (and (not (= (modulo n d) 0)) (inner_prime? n (+ d 1)))))
